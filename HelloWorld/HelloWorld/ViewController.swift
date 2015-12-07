76d5116f5a9a27460b63343e753e2a6b9bb30b16
gyoung <gregyoung@Susans-MacBook-Pro.local>
2015-12-06 16:21:30 -0800

    Put in Project
    Put the project in the right repo

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Greg Young on 12/1/15.
+//  Copyright © 2015 Greg Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
