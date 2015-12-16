bc155366a40b252edb0da121941bde6636f0cab6
Owen Pierce <opierce@gmail.com>
2015-12-15 20:24:55 -0800

    adding in class calculator activity

---
+++ Sessions/Session4/InClassActivity/InClassCalculator/Math.swift
@@ -0,0 +1,27 @@
+//
+//  Math.swift
+//  InClassCalculator
+//
+//  Created by Owen Pierce on 12/15/15.
+//  Copyright © 2015 Owen Pierce. All rights reserved.
+//
+
+import Foundation
+
+class Math: AnyObject {
+    func add(left: Double, right: Double) {
+        
+    }
+    
+    func subtract(left: Double, right: Double) {
+        
+    }
+    
+    func divide(left: Double, right: Double) {
+        
+    }
+    
+    func multiply(left: Double, right: Double) {
+        
+    }
+}
---
+++ Sessions/Session4/InClassActivity/InClassCalculator/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  InClassCalculator
+//
+//  Created by Owen Pierce on 12/15/15.
+//  Copyright © 2015 Owen Pierce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    /*
+    TODO: Using the attached Math class, implement
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division
+    
+    Then hook up the interface builder objects so that when we
+    tap one of the buttons, it will perform the selected math
+    function.
+    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
